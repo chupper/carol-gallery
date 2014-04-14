2069b297bbb2e097a4cc5f717d5f7f544b3d1f65
Karl Dowland <karl.dowland@gmail.com>
2014-04-14 07:57:22 -0400

    updated layout, added style

--- src/carol_gallery/views/home.clj
+++ src/carol_gallery/views/home.clj
@@ -5,4 +5,5 @@
   "Home page"
   (layout/main 
     "Carol Wu Homepage" 
-    [:h1 "This is the homepage"]))
+    [:div {:style "padding-top: 40px 15px; text-align: center;"} 
+     [:h1 "This is the homepage"]] ))
--- src/carol_gallery/views/layout.clj
+++ src/carol_gallery/views/layout.clj
@@ -7,6 +7,8 @@
     [:head 
      [:title title]
      [:link {:rel "stylesheet" :href "//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css"}]
+     [:link {:rel "stylesheet" :href "style.css"}]
      [:script {:src "//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"}]]
     [:body
-     [:div {:id "content"} body]]))
+     [:div {:class "navbar navbar-inverse navbar-fixed-top" :role "navigation"}]
+     [:div {:class "container"} body]]))
