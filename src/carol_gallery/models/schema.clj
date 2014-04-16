2b669349ac09f4d43139b413d3829b8a3f89654a
Karl Dowland <karl.dowland@gmail.com>
2014-04-16 08:04:32 -0400

    working on schema

--- src/carol_gallery/models/schema.clj
+++ src/carol_gallery/models/schema.clj
@@ -8,4 +8,14 @@
      :gallery
      [:id "varchar(32) PRIMARY KEY"]
      [:name "varchar(100)"]
-     [:description "varchar(512)"])))
+     [:description "varchar(512)"]
+     [:pictureid "varchar(32)"]
+     )))
+
+(defn create-picture-table []
+  (sql/with-connection db
+    (sql/create-table
+      :picture
+      [:id "varchar(32) PRIMARY KEY"]
+      [:filename "varchar(100)"]
+      [:blurb "varchar(512"])))
