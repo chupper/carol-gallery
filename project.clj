b66b0b04568224537f1c8dd5412bfa46d05ea007
Karl Dowland <karl.dowland@gmail.com>
2014-04-17 06:26:37 -0400

    Added functions to create db, fixed ns

--- project.clj
+++ project.clj
@@ -4,11 +4,13 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.6"]
                  [hiccup "1.0.5"]
-                 [org.clojure/java.jdbc "0.3.3"]]
+                 [org.clojure/java.jdbc "0.3.3"]
+                 [java-jdbc/dsl "0.1.0"]
+                 [postgresql/postgresql "8.4-702.jdbc4"]
+                 ]
   :plugins [[lein-ring "0.8.10"]]
   :ring {:handler carol-gallery.handler/app
-         :nrepl {:start? true :port 3001}
-         }
+         :nrepl {:start? true :port 3001}}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                         ]}})
--- src/carol_gallery/models/schema.clj
+++ src/carol_gallery/models/schema.clj
@@ -1,21 +1,30 @@
 (ns carol-gallery.models.schema
   (:require [carol-gallery.models.db :refer :all]
-            [clojure.java.jdbc :as sql]))
+            [clojure.java.jdbc :as jdbc]))
+
+(defn drop-all-tables []
+  (jdbc/db-do-commands db
+                       (jdbc/drop-table-ddl :picture)
+                       (jdbc/drop-table-ddl :gallery)))
+
+(defn create-all-tables []
+  (do (create-gallery-table)
+      (create-picture-table)))
 
 (defn create-gallery-table []
- (sql/with-connection db
-   (sql/create-table
-     :gallery
-     [:id "varchar(32) PRIMARY KEY"]
-     [:name "varchar(100)"]
-     [:description "varchar(512)"]
-     [:pictureid "varchar(32)"]
-     )))
+  (jdbc/db-do-commands db
+                       (jdbc/create-table-ddl
+                         :gallery
+                         [:id "varchar(32) PRIMARY KEY"]
+                         [:name "varchar(100)"]
+                         [:description "varchar(512)"]
+                         [:pictureid "varchar(32)"]
+                         )))
 
 (defn create-picture-table []
-  (sql/with-connection db
-    (sql/create-table
-      :picture
-      [:id "varchar(32) PRIMARY KEY"]
-      [:filename "varchar(100)"]
-      [:blurb "varchar(512"])))
+  (jdbc/db-do-commands db
+                       (jdbc/create-table-ddl 
+                         :picture
+                         [:id "varchar(32) PRIMARY KEY"]
+                         [:filename "varchar(100)"]
+                         [:blurb "varchar(512)"])))
