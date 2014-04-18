71fa54ff8649212f0d7d4575dc1bb2eb38db27f6
Karl Dowland <karl.dowland@gmail.com>
2014-04-18 19:28:49 -0400

    Updated hiccup template to use the function for js and css links

--- src/carol_gallery/views/layout.clj
+++ src/carol_gallery/views/layout.clj
@@ -6,9 +6,9 @@
   (page/html5
     [:head 
      [:title title]
-     [:link {:rel "stylesheet" :href "//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css"}]
-     [:link {:rel "stylesheet" :href "style.css"}]
-     [:script {:src "//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"}]]
+     (page/include-css "//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css"
+                       "style.css")
+     (page/include-js "//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js")]
     [:body
      [:div {:class "navbar navbar-inverse navbar-fixed-top" :role "navigation"}]
      [:div {:class "container"} body]]))
