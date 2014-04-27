f0e934d39be284ea1a58188a26fb03399b781f31
Karl Dowland <karl.dowland@gmail.com>
2014-04-26 21:03:28 -0400

    Added thumbnail code + imports

--- src/carol_gallery/models/picture.clj
+++ src/carol_gallery/models/picture.clj
@@ -1,12 +1,26 @@
 (ns carol-gallery.models.picture
   (require [clojure.java.jdbc :as jdbc]
-           [carol-gallery.models.db :refer :all]))
+           [carol-gallery.models.db :refer :all])
+  (import [java.awt.image AffineTransformOp BufferedImage]
+          [java.io FileInputStream ByteArrayOutputStream]
+          java.awt.RenderingHints
+          java.awt.geom.AffineTransform
+          javax.imageio.ImageIO))
 
-(defn to-byte-array  [f] 
-  (with-open  [input  (new java.io.FileInputStream f)
-               buffer  (new java.io.ByteArrayOutputStream)]
+(defn to-byte-array
+  "Converts a file input stream to a byte array." 
+  [f] 
+  (with-open [input  (FileInputStream. f)
+              buffer  (ByteArrayOutputStream.)]
     (clojure.java.io/copy input buffer)
     (.toByteArray buffer)))
+
+(defn scale
+  "Scales the image to create a thumbnail" 
+  [img ratio width height]
+  (let [scale (AffineTransform/getScaleInstance (double ratio) (double ratio))
+        transform-op (AffineTransformOp. scale AffineTransformOp/TYPE_BILINEAR)]
+    (.filter transform-op img (BufferedImage. width height (.getType img)))))
 
 (defn add-picture-record
   [gallery-id picture-bytes picture-name picture-description]
@@ -15,7 +29,7 @@
                  :galleryid (Integer. gallery-id)
                  :content (to-byte-array picture-bytes)
                  }]
-  (jdbc/insert! db :picture picture)))
+    (jdbc/insert! db :picture picture)))
 
 (defn read-picture-record-by-id [id]
   (first  (jdbc/query db ["SELECT * FROM picture WHERE id = ?", (Integer. id)])))
